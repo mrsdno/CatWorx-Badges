82bf18cedc701682c679ff4e68bc81482a23b919
mrsdno <christine@dno.dev>
2022-09-08 22:33:50 -0400

    initial hello world program

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// importing system Namespace (part of .NET framework)
+using System;
+
+// Namespaces are used to organize and provide a level of separation in the code.
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
